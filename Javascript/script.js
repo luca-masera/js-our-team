81d46f597afafc53ef28a12267a909a33c8abc94
luca-masera <lucamasera1995@gmail.com>
2023-10-23 15:06:41 +0200

    milestone 1

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,38 +2,47 @@
 const arrayOggetti = [
     
     {
-        name : 'Wayne Barnett';
-        role : 'Founder & CEO';
-        picture : 'wayne-barnett-founder-ceo.jpg'
-    }
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        picture : 'wayne-barnett-founder-ceo.jpg',
+    },
     
     {
-        name : 'Angela Caroll';
-        role : 'Chief Editor';
-        picture : 'angela-caroll-chief-editor.jpg'
-    }
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        picture : 'angela-caroll-chief-editor.jpg',
+    },
     
     {
-        name : 'Walter Gordon';
-        role : 'Office Manager';
-        picture : 'walter-gordon-office-manager.jpg'
-    }
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        picture : 'walter-gordon-office-manager.jpg',
+    },
     
     {
-        name : 'Angela Lopez';
-        role : 'Social Media Manager';
-        picture : 'angela-lopez-social-media-manager.jpg'
-    }
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        picture : 'angela-lopez-social-media-manager.jpg',
+    },
     
     {
-        name : 'Scott Estrada';
-        role : 'Developer';
-        picture : 'scott-estrada-developer.jpg'
-    }
+        name : 'Scott Estrada',
+        role : 'Developer',
+        picture : 'scott-estrada-developer.jpg',
+    },
     
     {
-        name : 'Barbara Ramos';
-        role : 'Graphic Designer';
-        picture : 'barbara-ramos-graphic-designer.jpg'
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        picture : 'barbara-ramos-graphic-designer.jpg',
     }
 ]
+
+for (let i = 0; i < arrayOggetti.length; i++){
+    console.group();
+    console.log(arrayOggetti[i].name);
+    console.log(arrayOggetti[i].role);
+    console.log(arrayOggetti[i].picture);
+    
+    console.groupEnd();
+}
